45a5bc136956a1b98daaad96dff7919031412112
goulven <goulven.furet@gmail.com>
2024-05-06 14:45:50 +0200

    Renaming to

--- xwiki-spring-boot-starter/src/test/java/org/open4goods/xwiki/XWikiServicesTest.java
+++ xwiki-spring-boot-starter/src/test/java/org/open4goods/xwiki/XWikiServicesTest.java
@@ -3,6 +3,7 @@ package org.open4goods.xwiki;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.open4goods.xwiki.services.XWikiHtmlService;
 import org.open4goods.xwiki.services.XWikiReadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
@@ -15,6 +16,24 @@ import org.springframework.test.context.ActiveProfiles;
 public class XWikiServicesTest {
 
 	@Autowired XWikiReadService xwikiReadService; 
+
+	@Autowired XWikiHtmlService xwikiHtmlService; 
+	
+	
+	
+	
+	
+	@Test
+	void testHtmlRendering() {
+		
+		String content = xwikiHtmlService.html("= Bienvenue sur votre Wiki =\n");
+		
+		
+		System.out.println(content);
+	}
+	
+	
+	
 	
 	@Test
 	void testEmpty() {
